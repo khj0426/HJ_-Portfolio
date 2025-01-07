03470cbe0e33ce2e151e0b0e8c287c5c9800dcbd
kimhyojung <706shin1728@naver.com>
2025-01-07 20:22:04 +0900

    chore: panda-css의존성 설치

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "./styles/style.css";
+import { css } from "../../styled-system/css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className={css({ bg: "red.400" })}>asd{children}</div>
+      </body>
     </html>
   );
 }
